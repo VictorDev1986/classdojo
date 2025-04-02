2e17dd2cc0f6a1063f06c06ba9914c1ff353f6fb
victordev1986 <victordev1986@gmail.com>
2025-04-01 21:32:27 -0500

    primer commit

---
+++ traducciones.js
@@ -0,0 +1,24 @@
+const traducciones = {
+    en: {
+        heading: "Complete the Sentence",
+        timeRemaining: "Time Remaining",
+        placeholder: "Write your answer",
+        submit: "Submit Answer",
+        questions: [
+            { text: "I like to play with the ball.", image: "ball.png" },
+            { text: "She loves singing in the rain.", image: "rain.png" },
+            { text: "He enjoys reading books.", image: "book.png" }
+        ],
+    },
+    es: {
+        heading: "Completa la Oración",
+        timeRemaining: "Tiempo Restante",
+        placeholder: "Escribe tu respuesta",
+        submit: "Enviar Respuesta",
+        questions: [
+            { text: "Me gusta jugar con la pelota.", image: "ball.png" },
+            { text: "A ella le encanta cantar bajo la lluvia.", image: "rain.png" },
+            { text: "A él le gusta leer libros.", image: "book.png" }
+        ],
+    }
+};
